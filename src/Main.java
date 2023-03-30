443d72c12d524efac2158d8daf707be8693ab88c
Sergey-Danilov-uazoarzt <uazoarzt@mail.ru>
2023-03-30 22:42:36 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Введите первое целое число: ");
+            int a = sc.nextInt();
+            System.out.println("Введите второе целое число: ");
+            int b = sc.nextInt();
+
+            if (a == b) {
+                System.out.println(a + " = " + b);
+            } else if (a < b) {
+                System.out.println(a + " < " + b);
+            } else if (a > b) {
+                System.out.println(a + " > " + b);
+            }
+    }
+}
